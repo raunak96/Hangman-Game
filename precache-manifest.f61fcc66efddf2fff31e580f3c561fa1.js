d2f349eb778babf13770a3b47fe74bb975feaef8
Raunak Kedia <raunak.kedia@walmartlabs.com>
2020-08-14 21:34:34 +0530

    Updates

---
+++ precache-manifest.f61fcc66efddf2fff31e580f3c561fa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ab931aed495f99218e20922e33b5ce",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "aa678c1d32100341d76a",
+    "url": "/hangman/static/css/main.1a70638b.chunk.css"
+  },
+  {
+    "revision": "bb2cde926df9d1715d21",
+    "url": "/hangman/static/js/2.097d8d9a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman/static/js/2.097d8d9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa678c1d32100341d76a",
+    "url": "/hangman/static/js/main.98ee61fd.chunk.js"
+  },
+  {
+    "revision": "7ef95293683d0ed87720",
+    "url": "/hangman/static/js/runtime-main.6b115b9b.js"
+  }
+]);
